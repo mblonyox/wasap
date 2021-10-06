1b48afc85184aaf7c10aca3710516819781840a7
Sukirno <mblonyox@gmail.com>
2021-10-06 16:14:38 +0700

    fix: tambahkan license dan metadata tambahan.

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="Wasap",
     version="1.0.0",
-    url="https://github.com/mblonyox/wasap",
     author="Sukirno",
     author_email="mblonyox@gmail.com",
     description="A service to send message via Whatsapp messenger programmatically.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords=["whatsapp", "chat", "message", "web"],
+    license="MIT",
+    url="https://github.com/mblonyox/wasap",
     packages=find_packages(),
+    python_requires=">=3.6.0",
     install_requires=["selenium", "click"],
     entry_points={
         "console_scripts": [
